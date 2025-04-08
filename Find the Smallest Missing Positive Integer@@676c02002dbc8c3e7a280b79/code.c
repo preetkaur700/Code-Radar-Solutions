c2ca7899bb22763d5f748cc070a8505c08f1c805
PARDEEP KAUR <pardeepkaur5948@gmail.com>
2025-04-08 10:32:11 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -12,20 +12,18 @@ int main() {
         scanf("%d", &arr[i]);
     }
 
-    // Create a boolean array to mark presence of positive numbers
-    bool present[N + 1]; // index 1 to N
+    
+    bool present[N + 1]; 
     for (int i = 0; i <= N; i++) {
         present[i] = false;
     }
 
-    // Mark the elements that are present in the array
     for (int i = 0; i < N; i++) {
         if (arr[i] > 0 && arr[i] <= N) {
             present[arr[i]] = true;
         }
     }
 
-    // Find the first index that is false
     for (int i = 1; i <= N; i++) {
         if (!present[i]) {
             printf("%d\n", i);
@@ -33,7 +31,6 @@ int main() {
         }
     }
 
-    // If all 1 to N are present, then the missing number is N+1
     printf("%d\n", N + 1);
     return 0;
 }
