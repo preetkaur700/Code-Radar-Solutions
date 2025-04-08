e088b5f12e81d3a3be7fd4c0c4e4954d9fa6685e
PARDEEP KAUR <pardeepkaur5948@gmail.com>
2025-04-08 10:43:58 +0530

    Solution to Find the First Repeating Element

--- Find the First Repeating Element@@676c02002dbc8c3e7a280b81/code.c
+++ Find the First Repeating Element@@676c02002dbc8c3e7a280b81/code.c
@@ -1,22 +1,20 @@
-// Your code here...
 #include <stdio.h>
 
-#define OFFSET 1000  // To handle negative numbers
-#define SIZE 2001    // Range from -1000 to 1000
+#define OFFSET 1000
+#define SIZE 2001
 
 int main() {
     int n;
     scanf("%d", &n);
 
     int arr[n];
-    int freq[SIZE] = {0};  // Frequency array initialized to 0
+    int freq[SIZE] = {0};
 
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
         freq[arr[i] + OFFSET]++;
     }
 
-    // Second pass to find the first repeating element
     for (int i = 0; i < n; i++) {
         if (freq[arr[i] + OFFSET] > 1) {
             printf("%d\n", arr[i]);
@@ -24,7 +22,6 @@ int main() {
         }
     }
 
-    // If no repeating element found
-    printf("No repeating element\n");
+    printf("-1\n");
     return 0;
 }
